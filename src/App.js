6c26c0dcffda85330da1b917854b9cadc7cf5148
symonne taylor <safiya@symonnes-mbp.home>
2021-07-04 14:11:49 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoForm from "./components/TodoForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="todo-App">
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
